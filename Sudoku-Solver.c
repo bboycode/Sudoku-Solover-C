b7bff4af273e4678d984819fc0e2a7aac1ba9482
Britton_laptop <25952277@sun.ac.za>
2023-06-29 21:06:00 +0200

    Prints 0 for board

--- Sudoku-Solver.c
+++ Sudoku-Solver.c
@@ -2,15 +2,55 @@
 #include <stdlib.h>
 int main(int argc, char *argv[])
 {	
+	int ROWS = 9;
+	int COLUMNS = 9;
 
-	int** board[9][9];
+	int board[COLUMNS][ROWS];
 
-	FILE* file; 
-
-	file = fopen(argv[0], "r");
+	FILE* file = fopen("test.txt", "r");
 	
-	int i;
-	for (i = 0; i < argv; ++i) {
+	if (file == NULL) {
+		printf("File could not be used.\n");
+		return 1;
+	}
+	
+	int row = 0; 
+
+	while (!feof(file)) {
+
+		if (ferror(file)) {
+
+			printf("Error reading file.\n");
+			return 1;
+		}
+		
+		for (int i =0; i < COLUMNS; i++) {
+
+			if(fscanf(file, "%n" , &board[row][i]) == EOF){
+				break;
+			}
+		
+		}
+
+		row ++; 
+		
+		if (row == ROWS) {
+			break;
+		}
+	}
+
+	fclose(file);
+
+	for (int i = 0 ; i < ROWS; i++) {
+		
+		for (int j = 0; j < COLUMNS; j++) {
+
+			printf("%d", board[i][j]);
+		
+		}
+
+		printf("\n");
+
 	}
 
 	return 0;
