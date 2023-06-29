7eae938a880fa5995df7097afb0a6a7fde96737f
Britton_laptop <25952277@sun.ac.za>
2023-06-29 09:15:24 +0200

    Added .C file

---
+++ Sudoku-Solver.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{	
+
+	int** board[9][9];
+
+	FILE* file; 
+
+	file = fopen(argv[0], "r");
+	
+	int i;
+	for (i = 0; i < argv; ++i) {
+	}
+
+	return 0;
+}
